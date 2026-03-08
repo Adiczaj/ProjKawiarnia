f4a1e4f3038844d5ab754d426ecb3c30e63f0b9a
Adiczaj <adamczajka173@interia.pl>
2026-03-08 19:36:27 +0000

    Login screen, firebase configuration

---
+++ kawiarnia/lib/app_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kawiarnia/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:kawiarnia/screens/auth/views/welcome_screen.dart';
+import 'package:kawiarnia/screens/home/views/home_screen.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Pizza Delivery',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(colorScheme: ColorScheme.light(background: Colors.grey.shade200, onBackground: Colors.black, primary: Colors.blue, onPrimary: Colors.white)),
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: ((context, state) {
+            if (state.status == AuthenticationStatus.authenticated) {
+              return const HomeScreen(); 
+            } else {
+              return const WelcomeScreen();
+            }
+          }),
+        ));
+  }
+}
--- kawiarnia/test/widget_test.dart
+++ kawiarnia/test/widget_test.dart
@@ -5,26 +5,23 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:kawiarnia/main.dart';
+import 'package:kawiarnia/app.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  setUpAll(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  });
+
+  testWidgets('App loads without errors', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(FirebaseUserRepo()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the app title is present.
+    expect(find.text('Kawiarnia'), findsOneWidget);
   });
 }
